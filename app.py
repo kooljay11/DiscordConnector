572b47aeeb91c8f94847ec57c0fe488b35e54048
kooljay11 <86942676+kooljay11@users.noreply.github.com>
2023-04-23 01:38:03 -0700

    cleaned up some things

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 # POTR-notes channel: 801953340736012339
 # POTR-bot testing channel: 1058195964491477062
 # UBC-commands spam channel: 810664831438684180
+# IML-ingame chat channel: 1018105092072816732
 import discord
 import asyncio
 import json
@@ -16,29 +17,22 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    # print(f"message: {message}")
-    # guild = await client.fetch_guild(message.guild.id)
-    # member = await guild.fetch_member(message.user.id)
-    # print(member.nick)
-    print(f"message.guild.name: {message.guild.name}")
-    print(
-        f"message.author.name: {message.author.name}#{message.author.discriminator}")
 
-    print(f"message.channel.id: {message.channel.id}")
+    # print(f"[{message.guild.name}] {message.author.nick}: {message.content}")
 
     # Ignore if the sender was this bot
     if message.author.id == 1099535052762787964:
         return
 
-    # print(f"message.author.nick: {message.author.nick}")
-
     with open("./channels.txt", "r") as file:
-        # channels = file.readlines()
         channels = file.read().split('\n')
 
-    # print(f"channels: {channels}")
     this_channel = str(message.channel.id)
     this_guild = message.guild.id
+    sender_name = message.author.nick
+
+    if sender_name == None:
+        sender_name = message.author.name + "#" + message.author.discriminator
 
     with open("guild_nicks.json", "r") as file:
         guilds = json.load(file)
@@ -52,8 +46,7 @@ async def on_message(message):
         channels.remove(this_channel)
         for channel in channels:
             destination_channel = client.get_channel(int(channel))
-            await destination_channel.send("[" + this_guild + "] " + message.author.nick + ": " + message.content)
-            # await destination_channel.send(message.author.name + "#" + message.author.discriminator + ": " + message.content)
+            await destination_channel.send("[" + this_guild + "] " + sender_name + ": " + message.content)
 
 
 async def main():
